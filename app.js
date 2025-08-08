1c4fd8d4494a0c6a16c5bb757e285f31c1e20b2c
Cristopher Castro <cristophercastro772@gmail.com>
2025-08-07 21:26:29 -0600

    alert and clean input, added a function to clean the input box after a name is entered and created an alert message if the input is blank

--- app.js
+++ app.js
@@ -2,18 +2,31 @@
 
 const mostrarLista = document.getElementById('listaAmigos');
 let arrayAmigos = [];
+let nombresAmigos = 0;
 
 function agregarAmigo() {
-    let nombresAmigos = document.getElementById('amigo').value;
-    arrayAmigos.push(nombresAmigos);
 
-    // limpiar el array antes de mostrar la lista 
-    listaAmigos.innerHTML = '';
-    // mostrar los nombres en forma de lista vertical 
-    for (let i of arrayAmigos) {
-        let li = document.createElement('li');
-        li.textContent = i;
-        mostrarLista.appendChild(li);
+    if (document.getElementById('amigo').value === '') {
+        alert('Por favor ingrese un nombre.')
+    } else {
+        let nombresAmigos = document.getElementById('amigo').value;
+        arrayAmigos.push(nombresAmigos);
+
+        // limpiar el array antes de mostrar la lista 
+        listaAmigos.innerHTML = '';
+        // mostrar los nombres en forma de lista vertical 
+        for (let i of arrayAmigos) {
+            let li = document.createElement('li');
+            li.textContent = i;
+            mostrarLista.appendChild(li);
+        }
+        console.log(arrayAmigos);
+        console.log(nombresAmigos);
+        limpiarInput();
     }
-    console.log(arrayAmigos);
+
+}
+
+function limpiarInput() {
+    let inputUsuario = document.getElementById('amigo').value = '';
 }
